f672cdee48e96d8d08d3098cff6eff519874d027
Usuario <Usuario@DESKTOP-7P0Q5TJ>
2023-11-14 13:20:29 -0600

    Agregar archivos de proyecto.

---
+++ WebAPIproyecto/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAPIproyecto.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
