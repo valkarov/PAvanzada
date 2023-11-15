b117347d72f7092c1be1dab3961e82bf91323630
Dalaray <mauro.david.alvarado@gmail.com>
2023-11-14 22:17:07 -0600

    a
    a

---
+++ WebProyecto/WebProyecto/Controllers/EmpleadoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProyecto.Entidades;
+using WebProyecto.Models;
+
+namespace WebProyecto.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        EmpleadosModel model = new EmpleadosModel();
+
+        [HttpGet]
+        public ActionResult InsertarEmpleado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertarEmpleado(EmpleadoENT e)
+        {
+            model.InsertarEmpleado(e);
+            return View();
+        }
+    }
+}
---
+++ WebProyecto/WebProyecto/Models/EmpleadosModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+using WebProyecto.Entidades;
+
+namespace WebProyecto.Models
+{
+    public class EmpleadosModel
+    {
+        public int InsertarEmpleado(EmpleadoENT e) 
+        {
+            //Llama al API
+            using (var http = new HttpClient()) 
+            {
+                string url = "https://localhost:44376/InsertarEmpleado";
+                JsonContent contenido = JsonContent.Create(e);
+
+                var resp = http.PostAsJsonAsync(url, contenido).Result;
+
+                if (resp.IsSuccessStatusCode)
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                else
+                    return 0;
+
+            }
+        }
+    }
+}
